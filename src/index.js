118b7320e955cbc4efe2a99c3a2a4eb0927df032
isaacgichuhi <zacmuturi45@gmail.com>
2023-10-05 00:34:19 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
 });
+
+
+let from=document.getElementById('create-task-form');
+
+from.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const input = document.querySelector('input#new-task-description');
+    const list = document.querySelector('ul#tasks');
+    const li = document.createElement('li');
+    let inputValue = input.value;
+    li.textContent = inputValue;
+    list.appendChild(li);
+    from.reset();
+
+})
